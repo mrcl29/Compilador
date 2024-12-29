bca35a39f979c44fa815d456ae1155e4d2180cd9
Marc2910llv <m.llobera@outlook.com>
2024-12-29 23:57:25 +0100

    errores semanticos

--- src/compiler/sintactic/Symbols/TaulaSimbols.java
+++ src/compiler/sintactic/Symbols/TaulaSimbols.java
@@ -1,7 +1,6 @@
 package compiler.sintactic.Symbols;
 
 import java.util.ArrayList;
-
 import compiler.sintactic.Symbols.Simbol.tipusDades;
 
 public class TaulaSimbols {
@@ -11,45 +10,47 @@ public class TaulaSimbols {
         taulaSimbols = new ArrayList<Simbol>();
     }
 
-    public void declararVariable(String id, tipusDades tipus, int fila, int columna) throws SemanticException {
+    public boolean declararVariable(String id, String tipus, int fila, int columna) throws Exception {
         Simbol simbol;
         for (int i = 0; i < taulaSimbols.size(); i++) {
             simbol = taulaSimbols.get(i);
-            if (simbol.getId() == id) {
-                throw new SemanticException("Ya existe una variable o constante con el nombre '" + id + "'");
+            if (simbol instanceof VariableConstant) {
+                if (simbol.getId() == id) {
+                    return false;
+                }
             }
         }
-        taulaSimbols.add(new VariableConstant(id, tipus, false, fila, columna));
+        tipusDades aux = tipusDades.valueOf(tipus);
+        taulaSimbols.add(new VariableConstant(id, aux, false, fila, columna));
+        return true;
     }
 
-    public void declararConstant(String id, tipusDades tipus, int fila, int columna) throws SemanticException {
+    public boolean declararConstant(String id, String tipus, int fila, int columna) throws Exception {
         Simbol simbol;
         for (int i = 0; i < taulaSimbols.size(); i++) {
             simbol = taulaSimbols.get(i);
-            if (simbol.getId() == id) {
-                throw new SemanticException("Ya existe una variable o constante con el nombre '" + id + "'");
+            if (simbol instanceof VariableConstant) {
+                if (simbol.getId() == id) {
+                    return false;
+                }
             }
         }
-        taulaSimbols.add(new VariableConstant(id, tipus, true, fila, columna));
+        tipusDades aux = tipusDades.valueOf(tipus);
+        taulaSimbols.add(new VariableConstant(id, aux, true, fila, columna));
+        return true;
     }
 
-    public void usarVariableConstant(String id, boolean assignacio) throws SemanticException {
+    public void usarVariableConstant(String id, boolean assignacio) throws Exception {
         VariableConstant simbol;
         for (int i = 0; i < taulaSimbols.size(); i++) {
             simbol = (VariableConstant) taulaSimbols.get(i);
             if (simbol.getId() == id) {
                 if (assignacio && simbol.isEsConstant()) {
-                    throw new SemanticException("No se puede assignar un valor nuevo a la constante '" + id + "'");
+                    throw new Exception("No se puede assignar un valor nuevo a la constante '" + id + "'");
                 }
                 return;
             }
         }
-        throw new SemanticException("La variable o constante con id '" + id + "' no ha sido declarada");
-    }
-
-    public static class SemanticException extends Exception {
-        public SemanticException(String message) {
-            super(message);
-        }
+        throw new Exception("La variable o constante con id '" + id + "' no ha sido declarada");
     }
 }
